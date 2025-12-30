972f495aabeca5b866a2667cf1bf4070ca208a2a
mohammadmohammadi785-crypto <alisafamohammadi785@gmail.com>
2025-12-30 18:18:02 +0430

    Some files Changed

---
+++ src/app/page.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function page() {
+  return (
+    <div>
+      <h1 className="text-center text-3xl font-bold text-gray-500 bg-green-500 p-4">
+        This is the Home page
+      </h1>
+    </div>
+  );
+}
